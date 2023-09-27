72245ebab5c840b3859efecf367a33d69f6e7ec6
Пользователь <mkomp06@gmail.com>
2023-09-27 11:46:05 +0300

    add template

--- start_1000_tasks/start_1000_tasks/template.h
+++ start_1000_tasks/start_1000_tasks/template.h
@@ -1,6 +1,11 @@
 #pragma once
-
-template<class T,class Function >
-T zd_1_2_3_4_5(int side,Function func) {
-	func()
+#include <iostream>
+template<typename T_int, typename T_double, class Function>
+T_double two_variables(T_double first, T_int second, Function func) {
+    return func(first, second);
+}
+int square_perimeter(int side)///1
+{
+    std::cout << two_variables(side, 0, [](int side, int variable_plug) {return side * 4; });///1
+    return 0;
 }
